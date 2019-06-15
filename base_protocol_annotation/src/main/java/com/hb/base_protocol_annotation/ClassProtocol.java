7688735ca5182b68838d688dbd3901fe1419e879
01380534 <biaohuang2@sf-express.com>
2019-06-15 22:22:57 +0800

    project init

---
+++ base_protocol_annotation/src/main/java/com/hb/base_protocol_annotation/ClassProtocol.java
@@ -0,0 +1,15 @@
+package com.hb.base_protocol_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author huangbiao
+ * @data 2019 2019-06-15 22:13
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface ClassProtocol {
+}
